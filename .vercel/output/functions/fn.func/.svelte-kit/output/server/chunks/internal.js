9841ad89192e7be1ab307f8d6e54a32ee83cda3a
eb1811 <emmanuilsborovikovs@outlook.com>
2024-07-25 11:20:52 +0100

    ads tag

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/chunks/internal.js
+++ .vercel/output/functions/fn.func/.svelte-kit/output/server/chunks/internal.js
@@ -115,7 +115,25 @@ const options = {
   root: Root,
   service_worker: false,
   templates: {
-    app: ({ head, body, assets: assets2, nonce, env }) => '<!DOCTYPE html>\n<html lang="en">\n	<head>\n		<meta charset="utf-8" />\n		<link rel="icon" href="' + assets2 + '/favicon.png" />\n		<meta name="viewport" content="width=device-width" />\n		' + head + '\n	</head>\n	<body data-sveltekit-preload-data="hover">\n		<div style="display: contents">' + body + "</div>\n	</body>\n</html>\n",
+    app: ({ head, body, assets: assets2, nonce, env }) => '<!doctype html>\n<html lang="en">\n  <head>\n    <meta charset="utf-8" />\n    <link rel="icon" href="' + assets2 + '/favicon.png" />\n    <meta name="viewport" content="width=device-width" />\n    ' + head + `
+
+    <!-- Google tag (gtag.js) -->
+    <script
+      async
+      src="https://www.googletagmanager.com/gtag/js?id=AW-11379785830"
+    ><\/script>
+    <script>
+      window.dataLayer = window.dataLayer || []
+      function gtag() {
+        dataLayer.push(arguments)
+      }
+      gtag('js', new Date())
+
+      gtag('config', 'AW-11379785830')
+    <\/script>
+  </head>
+  <body data-sveltekit-preload-data="hover">
+    <div style="display: contents">` + body + "</div>\n  </body>\n</html>\n",
     error: ({ status, message }) => '<!doctype html>\n<html lang="en">\n	<head>\n		<meta charset="utf-8" />\n		<title>' + message + `</title>
 
 		<style>
@@ -187,7 +205,7 @@ const options = {
 		<div class="error">
 			<span class="status">` + status + '</span>\n			<div class="message">\n				<h1>' + message + "</h1>\n			</div>\n		</div>\n	</body>\n</html>\n"
   },
-  version_hash: "1fbsbzs"
+  version_hash: "nbvqr4"
 };
 async function get_hooks() {
   return {};
--- .vercel/output/functions/fn.func/.svelte-kit/vercel-tmp/manifest.js
+++ .vercel/output/functions/fn.func/.svelte-kit/vercel-tmp/manifest.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set([".DS_Store","favicon.png","features-blue.svg","features-emerald.svg","features-indigo.svg"]),
 	mimeTypes: {".png":"image/png",".svg":"image/svg+xml"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.Coz_KG44.js","app":"_app/immutable/entry/app.5YGM7f01.js","imports":["_app/immutable/entry/start.Coz_KG44.js","_app/immutable/chunks/entry.D2y6kFvK.js","_app/immutable/chunks/scheduler.BvLojk_z.js","_app/immutable/entry/app.5YGM7f01.js","_app/immutable/chunks/scheduler.BvLojk_z.js","_app/immutable/chunks/index.aKaDqPNn.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
+		client: {"start":"_app/immutable/entry/start.BZ5ubx5T.js","app":"_app/immutable/entry/app.BEGqU7Pe.js","imports":["_app/immutable/entry/start.BZ5ubx5T.js","_app/immutable/chunks/entry.DTOYNZlD.js","_app/immutable/chunks/scheduler.BvLojk_z.js","_app/immutable/entry/app.BEGqU7Pe.js","_app/immutable/chunks/scheduler.BvLojk_z.js","_app/immutable/chunks/index.aKaDqPNn.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
 		nodes: [
 			__memo(() => import('../output/server/nodes/0.js')),
 			__memo(() => import('../output/server/nodes/1.js'))
